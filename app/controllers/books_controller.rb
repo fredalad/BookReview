03ad0a09c9bc136f632e35d8b6bdefa22cea6dd3
fredalad <nick.manfreda885@gmail.com>
2016-08-23 13:14:39 -0600

    third commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,17 @@
 class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
+
   def index
-    @books = Book.all.order("created_at DESC")
+    if params[:category].blank?
+      @books = Book.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @books = Book.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 
   def show
 
-  end
-
-  def edit
-
-  end
-
-  def update
-    if @book.update(book_params)
-      redirect_to book_path(@book)
-    else
-      render 'edit'
-    end
   end
 
   def destroy
@@ -26,12 +20,13 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
+    @book = current_user.books.build
+    @categories = Category.all.map{ |c| [c.name, c.id]}
   end
 
-
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
     if @book.save
       redirect_to root_path
     else
@@ -39,10 +34,24 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @categories = Category.all.map{ |c| [c.name, c.id]}
+  end
+
+  def update
+    @book.category_id = params[:category_id]
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+
+
   private
 
     def book_params
-      params.require(:book).permit(:title, :description, :author)
+      params.require(:book).permit(:title, :description, :author, :category_id)
     end
 
     def find_book
